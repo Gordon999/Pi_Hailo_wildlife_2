cfcdea62838e25264257693b98b68375e650af8b
Gordon999 <Gordon999@users.noreply.github.com>
2024-09-09 10:32:53 +0100

    v0.03

--- detect_002.py
+++ detect_002.py
@@ -2,7 +2,7 @@
 
 """Example module for Hailo Detection."""
 
-#v0.02
+#v0.03
 
 import argparse
 import cv2
@@ -17,15 +17,19 @@ import glob
 import datetime
 import shutil
 
+# detection objects
+objects = ["cat","bear","bird"] # maximum 3
+
 # set variables
-v_width    = 1456 # video width
-v_height   = 1088 # video height
-v_length   = 10   # seconds, minimum video length
-pre_frames = 5    # seconds,  defines length of pre-trigger buffer
-fps        = 25   # video frame rate
-mp4_fps    = 25   # mp4 frame rate
-mp4_timer  = 30   # seconds, convert h264s to mp4s after this time if no detections
-mp4_anno   = 1    # apply timestamps to video, 1 = yes, 0 = no
+v_width      = 1456 # video width
+v_height     = 1088 # video height
+v_length     = 10   # seconds, minimum video length
+pre_frames   = 5    # seconds, defines length of pre-trigger buffer
+fps          = 25   # video frame rate
+mp4_fps      = 25   # mp4 frame rate
+mp4_timer    = 30   # seconds, convert h264s to mp4s after this time if no detections
+mp4_anno     = 1    # show timestamps on video, 1 = yes, 0 = no
+show_detects = 0    # show detections on video, 1 = yes, 0 = no
 
 # mp4_annotation parameters
 colour    = (255, 255, 255)
@@ -37,7 +41,7 @@ thickness = 2
 # shutdown time
 sd_hour = 20
 sd_mins = 30
-auto_sd = 0 # set to 1 to shutdown at set time
+auto_sd = 0  # set to 1 to shutdown at set time
 
 # ram limit
 ram_limit = 150 # stops recording if ram below this
@@ -57,7 +61,6 @@ def extract_detections(hailo_output, w, h, class_names, threshold=0.5):
                 bbox = (int(x0 * w), int(y0 * h), int(x1 * w), int(y1 * h))
                 results.append([class_names[class_id], bbox, score])
     return results
-
 
 def draw_objects(request):
     current_detections = detections
@@ -92,6 +95,29 @@ if __name__ == "__main__":
     h_user = "/home/" + os.getlogin( )
     m_user = "/media/" + os.getlogin( )
 
+    # read coco.txt file and set detection object numbers
+    names = []
+    a = -1
+    b = -1
+    c = -1
+    with open('/home/' + user + '/picamera2/examples/hailo/coco.txt', "r") as file:
+       line = file.readline()
+       while line:
+          names.append(line.strip())
+          line = file.readline()
+    for x in range(0,len(objects)):
+        for y in range(0,len(names)):
+            if objects[x] == names[y]:
+                if a == -1:
+                    a = y
+                elif b == -1:
+                    b = y
+                elif c == -1:
+                    c = y
+    a = max(a,0)
+    b = max(b,0)
+    c = max(c,0)
+
     # check if clock synchronised
     if "System clock synchronized: yes" in os.popen("timedatectl").read().split("\n"):
         synced = 1
@@ -125,7 +151,7 @@ if __name__ == "__main__":
             main = {'size': (video_w, video_h), 'format': 'XRGB8888'}
             lores = {'size': (model_w, model_h), 'format': 'RGB888'}
             controls = {'FrameRate': fps}
-            # use next line instead of above if using pi v3 camera
+            # use next line instead of above if using Pi v3 camera with AF
             #controls = {'FrameRate': fps,"AfMode": controls.AfModeEnum.Continuous,"AfTrigger": controls.AfTriggerEnum.Start}
             config = picam2.create_preview_configuration(main, lores=lores, controls=controls)
             picam2.configure(config)
@@ -136,8 +162,8 @@ if __name__ == "__main__":
             picam2.start()
             picam2.start_encoder(encoder)
             encoding = False
-            # enable the next line to show detections on video
-            #picam2.pre_callback = draw_objects
+            if show_detects == 1:
+                picam2.pre_callback = draw_objects
 
             # Process each low resolution camera frame.
             while True:
@@ -150,28 +176,29 @@ if __name__ == "__main__":
 
                 # Run inference on the preprocessed frame
                 results = hailo.run(frame)
-
+                
                 # Extract detections from the inference results
-                detections = extract_detections(results[0], video_w, video_h, class_names, args.score_thresh)
+                if show_detects == 1:
+                    detections = extract_detections(results[0], video_w, video_h, class_names, args.score_thresh)
 
                 # detection
-                if len(results[0][15]) != 0 or len(results[0][21]) != 0 or len(results[0][14]) != 0: # cat,bear or bird
+                if len(results[0][a]) != 0 or len(results[0][b]) != 0 or len(results[0][c]) != 0:
                     start = time.monotonic()
                     start2 = time.monotonic()
                     # start recording
-                    if not encoding:
+                    if not encoding and freeram > ram_limit:
                         now = datetime.datetime.now()
-                        timestamp = now.strftime("%y%m%d%H%M%S")
+                        timestamp = now.strftime("%y%m%d_%H%M%S")
                         encoder.output.fileoutput = "/run/shm/" + str(timestamp) + '.h264'
                         encoder.output.start()
                         encoding = True
-                        print("New Detection", timestamp)
+                        print("New  Detection", timestamp)
 
                 # stop recording
-                if encoding and time.monotonic() - start > v_length or freeram < ram_limit:
+                if encoding and (time.monotonic() - start > v_length or freeram < ram_limit):
                     now = datetime.datetime.now()
-                    timestamp2 = now.strftime("%y%m%d%H%M%S")
-                    print("Stopped", timestamp2)
+                    timestamp2 = now.strftime("%y%m%d_%H%M%S")
+                    print("Stopped Record", timestamp2)
                     encoder.output.stop()
                     encoding = False
                     start2 = time.monotonic()
@@ -188,37 +215,39 @@ if __name__ == "__main__":
                         os.system(cmd)
                         os.remove(h264s[x])
                         print("Saved",h264s[x][:-5] + '.mp4')
-                    Videos = glob.glob('/run/shm/2???????????.mp4')
+                    Videos = glob.glob('/run/shm/2?????_??????.mp4')
                     Videos.sort()
                     # move Video RAM Files to SD card
                     for xx in range(0,len(Videos)):
                         if not os.path.exists(h_user + "/" + '/Videos/' + Videos[xx]):
                             shutil.move(Videos[xx], h_user + '/Videos/')
 
-                # check if clock synchronised
-                if "System clock synchronized: yes" in os.popen("timedatectl").read().split("\n"):
-                    synced = 1
-                else:
-                    synced = 0
-                # check current hour and shutdown
-                now = datetime.datetime.now()
-                sd_time = now.replace(hour=sd_hour, minute=sd_mins, second=0, microsecond=0)
-                if now >= sd_time and time.monotonic() - startup > 300 and synced == 1 and auto_sd == 1:
-                    # move mp4s to USB if present
-                    USB_Files  = []
-                    USB_Files  = (os.listdir(m_user))
-                    if len(USB_Files) > 0:
-                        usedusb = os.statvfs(m_user + "/" + USB_Files[0] + "/")
-                        USB_storage = ((1 - (usedusb.f_bavail / usedusb.f_blocks)) * 100)
-                    if len(USB_Files) > 0 and USB_storage < 90:
-                        Videos = glob.glob(h_user + '/Videos/*.mp4')
-                        Videos.sort()
-                        for xx in range(0,len(Videos)):
-                            movi = Videos[xx].split("/")
-                            if not os.path.exists(m_user + "/" + USB_Files[0] + "/Videos/" + movi[4]):
-                                shutil.move(Videos[xx],m_user + "/" + USB_Files[0] + "/Videos/")
-                    time.sleep(5)
-                    # shutdown
-                    os.system("sudo shutdown -h now")
+                    # auto shutdown
+                    if auto_sd == 1:
+                        # check if clock synchronised
+                        if "System clock synchronized: yes" in os.popen("timedatectl").read().split("\n"):
+                            synced = 1
+                        else:
+                            synced = 0
+                        # check current hour and shutdown
+                        now = datetime.datetime.now()
+                        sd_time = now.replace(hour=sd_hour, minute=sd_mins, second=0, microsecond=0)
+                        if now >= sd_time and time.monotonic() - startup > 300 and synced == 1:
+                            # move mp4s to USB if present
+                            USB_Files  = []
+                            USB_Files  = (os.listdir(m_user))
+                            if len(USB_Files) > 0:
+                                usedusb = os.statvfs(m_user + "/" + USB_Files[0] + "/")
+                                USB_storage = ((1 - (usedusb.f_bavail / usedusb.f_blocks)) * 100)
+                            if len(USB_Files) > 0 and USB_storage < 90:
+                                Videos = glob.glob(h_user + '/Videos/*.mp4')
+                                Videos.sort()
+                                for xx in range(0,len(Videos)):
+                                    movi = Videos[xx].split("/")
+                                    if not os.path.exists(m_user + "/" + USB_Files[0] + "/Videos/" + movi[4]):
+                                        shutil.move(Videos[xx],m_user + "/" + USB_Files[0] + "/Videos/")
+                            time.sleep(5)
+                            # shutdown
+                            os.system("sudo shutdown -h now")
 
 
